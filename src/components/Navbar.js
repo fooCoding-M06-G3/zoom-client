2c363ca774a5024c65cbdeed76e36b58a638c11f
Rawaa Al Kabbani <rawaa.alkabbani@outlook.com>
2020-02-28 21:09:19 +0100

    Remove unused import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Layout, Navigation, Header, Textfield } from 'react-mdl';
 import { NavLink } from 'react-router-dom';
 
