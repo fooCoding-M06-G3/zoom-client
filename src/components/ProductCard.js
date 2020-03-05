ad6ed4ecc5f6290028ed86ce8fd156969c95ec71
abdulrauf84 <abdul.rauf@live.se>
2020-03-05 04:35:32 +0100

    product card styles updated

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,29 +1,25 @@
 import React from 'react'
 import '../App.css';
-import { Card, CardTitle, CardActions, CardText, MenuItem } from 'react-mdl'
+import { Card, CardTitle, CardActions, } from 'react-mdl'
 
 export default function ProductCard() {
-
   return (
-
     <Card shadow={0} style={{ width: '23%', height: '36vh', margin: '10px', }} >
       <CardTitle expand style={{ background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }} >
         <CardActions style={{ width: '30%', background: '#333', marginTop: '70%' }}>
           <span style={{ width: '100%', color: '#fff', padding: 5, fontSize: '1.4em', fontWeight: '500' }}>
-            1200
+            1099
           </span>
         </CardActions>
       </CardTitle>
-
       <div style={{ display: 'flex', height: '20%', width: '100%' }}>
         <span className='itemText' >
           Item Name
           </span>
-        <span className='itemText' >
+        <span className='itemText'>
           City
           </span>
       </div>
     </Card>
-
   )
 }
